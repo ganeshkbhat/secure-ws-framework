d75b6c9291d05992e53e5d3d73f254d50ad0b255
Ganesh B <ganeshsurfs@gmail.com>
2024-03-14 07:20:48 +0530

    [COMMIT] First commit secure was framework

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * Package: 
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i  --save
+ * Github: https://github.com/ganeshkbhat/
+ * npmjs Link: https://www.npmjs.com/package/
+ * File: index.js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+// https://github.com/jtarchie/some-router/tree/main
+// https://www.npmjs.com/package/ws
+
